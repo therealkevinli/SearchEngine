810fff790beb6c600ce23e8daebe38315280541e
Georgio <georgio4u@gmail.com>
2019-03-07 09:24:02 -0800

    web gui and endpoint

---
+++ search.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+import QueryDB
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/search", methods=['POST'])
+def query_the_search():
+    try:
+        data = request.data
+        dataDict = json.loads(data)
+        query = dataDict['query']
+        print( query )
+        results = QueryDB.search_query(query, 50)
+        return jsonify(results), 200
+    except Exception as e:
+        return jsonify({'message' : 'error reading query'}), 500
+
+if __name__ == '__main__':
+    app.run()
---
+++ web/main.js
@@ -0,0 +1,49 @@
+function loadTable(jsonData, query) {
+    console.log("populating query results into table");
+    //let jsonData = JSON.parse(data);
+
+    let resultsTableBodyElement = $('#resultsTableBody');
+
+    for( let i = 0; i < jsonData.length; ++i ) {
+        let rowHTML = "";
+        rowHTML += '<tr>';
+        rowHTML += '<td>';
+        rowHTML += '<a href="' + jsonData[i] + '">' + jsonData[i] + '</a>';
+        rowHTML += '</td>';
+        rowHTML += '</tr>';
+        resultsTableBodyElement.append(rowHTML);
+    }
+}
+
+function handleNormalSearch(query) {
+    $('#resultsTableBody').empty();
+    console.log("Doing normal search with query: " + query);
+
+    $.ajax({
+        "method": "POST",
+        "url"   : "http://127.0.0.1:5000/api/search",
+        "data"  : JSON.stringify( {"query" : query} ),
+        "contentType" : "application/json;charset=UTF-8",
+        "success": function(data) {
+            if( data == null) data = [];
+            console.log("Ajax success. Result set size: " + data.length)
+            loadTable(data, query);
+        }, 
+        "error" : function(errData) {
+            console.log("lookup ajax error");
+            console.log(errData);
+        }
+    });
+}
+
+$('#search').keypress(function(event) {
+    if(event.keyCode == 13) {
+        event.preventDefault();
+        handleNormalSearch($('#search').val());
+    }
+});
+
+$('#search-button').click(function(event) {
+    event.preventDefault();
+    handleNormalSearch($('#search').val());
+});
