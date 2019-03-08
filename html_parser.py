8a6664ac8ffcf5cf15c9e6be2772da155c728427
therealkevinli <kevin.m.li888@gmail.com>
2019-03-07 23:40:05 -0800

    Update html_parser.py

--- html_parser.py
+++ html_parser.py
@@ -3,6 +3,7 @@ import os
 from html.parser import HTMLParser
 from collections import defaultdict
 
+stopwords = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'}
 
 class MyHTMLParser(HTMLParser):
 
@@ -13,10 +14,12 @@ class MyHTMLParser(HTMLParser):
         self.tag_dict = defaultdict(bool)
         self.tag_weights = {"body": 1,"title": 10, "h1": 6,\
                             "h2": 5, "h3": 4, "b": 4, "strong": 4}
-        self.tagStack = []
+        self.tagStack = ['title']
         
     
     def handle_starttag(self, tag, attrs):
+        if len(self.tagStack) > 0 and self.tagStack[0] == 'title':
+            self.tagStack.pop()
         self.tagStack.append(tag)
 
 
@@ -26,10 +29,10 @@ class MyHTMLParser(HTMLParser):
 
 
     def handle_data(self, data):
-        tokens = "".join((char if char.isalnum() else " ") for char in line).split()
+        tokens = "".join((char if char.isalnum() else " ") for char in data).split()
         # Lowercase all tokens
         tokens = [token.lower() for token in tokens]
-
+        tokens = [token for token in tokens if token not in stopwords]#this removes all the stop words
         for token in tokens:
             #countDict[word] += 1    
             for tag in self.tagStack:
